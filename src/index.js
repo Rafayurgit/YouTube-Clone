ba7abe73408817a1b86c044f625eb3ad73eef687
farruop <rafayKhanpvt@gmail.com>
2024-10-18 22:54:40 +0530

    added dotenv config which is an advance and new feature at this movement that's why we used experimental in the dev script of package.json file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from './constants';
-import express from 'express';
+//require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv';
+import connectDb from './db/index.js';
 
 
-const app= express();
-;( async()=>{
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+dotenv.config({
+    path:'./env'
+})
+
+connectDb();
+
+
+// const app= express();
+// ;( async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         
-        app.on("Error:", (error)=>{
-            console.log("ERR:" ,error)
-            throw error
-        })
+//         app.on("Error:", (error)=>{
+//             console.log("ERR:" ,error)
+//             throw error;
+//         })
 
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listining on port ${process.env.PORT}`);
+//         app.listen(process.env.PORT , ()=>{
+//             console.log(`App is listining on port ${process.env.PORT}`);
             
-        })
+//         })
 
-    }catch(error){
-        console.error("ERROR", error )
-        throw err
-    }
-})()
+//     }catch(error){
+//         console.error("ERROR", error )
+//         throw error
+//     }
+// })()
