641674963ba55a6c3a0e12e75f435cef3fbc8c6a
farruop <rafayKhanpvt@gmail.com>
2024-10-19 21:58:32 +0530

    used  mongooseAggregatePaginate, bcrypt and jwt in propper format created custom hooks and methods for that

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
+
 const videoSchema= new mongoose.Schema({
     videoFile:{
         type:String, //cloudinary url
@@ -37,6 +38,6 @@ const videoSchema= new mongoose.Schema({
 },
 {timestamps:true})
 
-mongoose.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video= mongoose.model("Video", videoSchema);
