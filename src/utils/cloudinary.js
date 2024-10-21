3264199efef8b9a8a9d4fc3b2d287478e3ec3c4a
farruop <rafayKhanpvt@gmail.com>
2024-10-21 21:28:04 +0530

    created uploadOnCloudinray methond for file upload in cloudinary util file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import {v2 as cloudinary} from "cloudinary"
+import { log } from "console"
 import fs from "fs"
 
 
@@ -7,3 +8,18 @@ cloudinary.config({
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET,
 })
+
+const uploadOnCloudinary= async (localFilePath)=>{
+    try {
+        if(!localFilePath)return null
+        const response=await cloudinary.uploader.upload(localFilePath, {
+            resource_type:"auto"
+        })
+        console.log("File uploaded Successfully", response.url);
+        return response
+        
+    } catch (error) {
+        fs.unlink(localFilePath)
+        return null;
+    }
+}
