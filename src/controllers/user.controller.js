f1fa571f9579ce2b5584e7f8426312197cc6ec4c
farruop <rafayKhanpvt@gmail.com>
2024-10-24 22:27:46 +0530

    wrote code for uploading avatar and cover image some validation and user entry on db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,48 @@
 import {asyncHandler} from "../utils/asyncHandeler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js "
+import {uploadOnCloudinary} from"../utils/cloudinary.js ";
+
 
 const registerUser = asyncHandler( async (req, res)=>{
-    res.status(200).json({
-        message:"Ok"
+    
+    const {fullname, username, email, password}= req.body
+    console.log("email:", email);
+
+
+    if(
+        [fullname, username, email, password].some((field)=>
+            field?.trim() ===""
+        )
+    ){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser =User.findOne({
+        $or:[{username}, {email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "user with email or username already exist");
+        
+    }
+
+    const avatarLocalPath= req.files?.avatar[0]?.path;
+    const coverImageLocalPath= req.files?.coverImage[0].path;
+
+    const avatar =await uploadOnCloudinary(avatarLocalPath)
+    const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError( 409 "Avatar is required");
+    }
+
+     const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
     })
 })
 
