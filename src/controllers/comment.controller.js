81af20e62ed2560460d7061257f7015ae2b8f594
farruop <rafayKhanpvt@gmail.com>
2024-11-01 23:21:46 +0530

    Add functionality of getVideoComments in comments controller

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+import {Comment} from "../models/comment.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId}= req.params;
+    const {page=1, limit=10}= req.query;
+
+    if(!mongoose.Types.ObjectId.isValid(videoId)){
+        throw new ApiError(400,"Invalid video Id")
+    }
+
+    const comments =await Comment.find({video: videoId})
+    .skip((page-1)*limit)
+    .limit(parseInt(limit))
+    .sort({createdAt: -1})
+
+    return res.status(200)
+    .json(new ApiResponse (200, comments, "Comments fetched successfully"))
+})
+
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+     deleteComment
+    }
