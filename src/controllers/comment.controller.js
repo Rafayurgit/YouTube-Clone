059bedf5d70ba0311615948e992251e221ccb812
farruop <rafayKhanpvt@gmail.com>
2024-11-03 22:01:01 +0530

    Add: delete functionality in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -71,7 +71,25 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
-    
+    const {commentId}= req.params;
+    const userId= req.user._id;
+
+    if(!mongoose.Types.ObjectId.isValid(commentId)){
+        throw new ApiError(400,"Invalid Comment Id");
+    }
+
+    const comment=await Comment.findOneAndDelete(
+        {
+            _id:commentId,
+            owner:userId
+        }
+    )
+    if (!comment) {
+        throw new ApiError(404, "Comment not found or not authorized");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, null, "Comment deleted successfully"))
 
 })
 
