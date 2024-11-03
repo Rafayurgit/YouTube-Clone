5691511d319c6237f378de521af5f0d520bc89d9
farruop <rafayKhanpvt@gmail.com>
2024-11-03 21:51:37 +0530

    Add: update functionality to comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -45,10 +45,34 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId}= req.params;
+    const {content} = req.body;
+    const userId= req.user._id;
+
+    if(!mongoose.Types.ObjectId.isValid(commentId)){
+        throw new ApiError(400,"Invalid video ID");
+    }
+
+    const comment =await Comment.findOneAndUpdate(
+        {_id:commentId, owner:userId},
+        {content:content},
+        {new :true}
+    )
+
+    if(!comment){
+        throw new ApiError(404, "Comment not found or not authorized");
+        
+    }
+    
+    return res.status(200)
+    .json(new ApiResponse(200, comment, "Comment updated successfully"))
+
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    
+
 })
 
 export {
