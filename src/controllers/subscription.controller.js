9fbf16b9cc8b7f2761e41c50171399fe4c1fa2c9
farruop <rafayKhanpvt@gmail.com>
2024-11-21 21:45:44 +0530

    Add: list subscribers functionality

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -29,7 +29,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         await Subscription.deleteOne({_id:existingSubscription})
         return res.status(200).json(new ApiResponse(200, null, "unsubscribed successfully"))
     }else{
-        const newSubscriber =await subscriber.create({
+        const newSubscriber =await Subscription.create({
             channel:channelId,
             subscriber:userId
         })
@@ -41,6 +41,22 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const channel= await User.findById(channelId)
+    if(!channel){
+        throw new ApiError(404,"Channel not found");
+    }
+
+    const subscribers= await Subscription.find({channel:channelId}).populate("subscriber", "name email")
+    if(!subscribers.length){
+        return res.status(200).json(new ApiResponse(200, [], "No subscribers found for this channel"))
+    }else{
+        return res.status(200).json(new ApiResponse(200, subscribers, "subscribers fetched successfully"))
+    }
 })
 
 // controller to return channel list to which user has subscribed
