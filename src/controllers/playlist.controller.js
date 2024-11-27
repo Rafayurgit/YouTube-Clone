65ade9be7f36e66e9bce8447a4dd7904559b8719
farruop <rafayKhanpvt@gmail.com>
2024-11-27 23:09:30 +0530

    Add: getPlaylistById finctionality

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -7,18 +7,32 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
-
     //TODO: create playlist
+
+
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400,"Invalid playList Id");
+    }
+
+    const playlist =await Playlist.findById(playlistId)
+    if(!playlist){
+        throw new ApiError(404,"Playlist not found").populate('owner', 'name email');
+        
+    }
+    return res.status(200).json(new ApiResponse(200, playlist, "PlayList fetched successfully"))
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
