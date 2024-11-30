11eb31095f8522c4892930f908f2d7def43e7d72
farruop <rafayKhanpvt@gmail.com>
2024-11-30 22:13:29 +0530

    Add: getChannelStatus functionality

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,11 +8,58 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const {channelId}= req.params;
+
+    if(!mongoose.isValidObjectId(channelId)){
+        throw new ApiError(400,"Invalid channel id");
+    }
+
+    const totalViews=await Video.aggregate([
+        {$match:{owner: mongoose.Types.ObjectId(channelId)}},
+        {$group: {_id:null, totalViews: {$sum:"$views"}}},
+    ]);
+
+    const views= totalViews.length>0 ? totalViews[0].totalViews : 0;
+
+    const totalSubscribers=await Subscription.countDocuments({channelId});
+
+    const totalVideos= await Video.countDocuments({owner:channelId});
+
+    const totalLikes= await Like.aggregate([
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videoId",
+                foreignField: "_id",
+                as: "video",
+            },
+            
+        },
+        {
+            $match:{"video.owner": mongoose.Types.ObjectId(channelId)},
+        },
+        {
+            $group:{_id: null , totalLikes :{$sum:1}},
+        },
+    ]);
+
+    const likes= totalLikes.length>0 ? totalLikes[0].totalLikes :0;
+
+    const stats={
+        totalViews: views,
+        totalSubscribers,
+        totalVideos,
+        totalLikes:likes
+    };
+
+    return res.status(200).json(new ApiResponse(200, stats, "Channel status fetched successfully"))
+
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
-    const channelId= req.params;
+    const {channelId}= req.params;
 
     if(!mongoose.isValidObjectId(channelId)){
         throw new ApiError(400,"Invalid Channel Id");
@@ -20,12 +67,12 @@ const getChannelVideos = asyncHandler(async (req, res) => {
 
     const videos= await Video.find({owner:channelId});
     if(videos.length=== 0){
-        throw new ApiError(404,"No videos found for this channel");
+        return res
+        .status(200)
+        .json(new ApiResponse(200, [], "No videos found for this channel"));
     }
 
     return res.status(200).json(new ApiResponse(200, videos, "Channel videos fetched successfully"))
-
-
 })
 
 export {
